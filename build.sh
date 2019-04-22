50f9b404bd685556fd39a59236085899763e83c9
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-04-22 18:49:43 -0500

    MANDA-448: adding build env vars and cleanup var name

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-docker_version=$(cat "$curDir"/VERSION)
+docker_version=$(cat "${__dir}"/VERSION)
 
 echo "building version: ${docker_version} locally"
 
-docker build --build-arg PCF_CLI_VERSION="${docker_version}" --pull -t docker-cicd-pcf:"${docker_version}" -t docker-cicd-pcf:latest .
+docker build --build-arg PCF_CLI_VERSION="${docker_version}" --pull -t docker-cicd-pcf:"${docker_version}" -t docker-cicd-pcf:latest "${__dir}"
 # docker push mrllsvc/pcf-tools:"${docker_version}"
 # docker push mrllsvc/pcf-tools:latest
