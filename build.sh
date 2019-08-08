38a6e57e134b1a0f871fbf6111eb376a96d8a79b
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-08-07 23:49:06 -0500

    MANDA-1049: adding hooks (#14)
    Add support for hooks during deploy

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 readonly __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+pcf_cli_version=$(sed s/[-a-zA-Z]*$// <"${__dir}"/VERSION)
 docker_version=$(cat "${__dir}"/VERSION)
 
 echo "building version: ${docker_version} locally"
 
-docker build --build-arg PCF_CLI_VERSION="${docker_version}" --pull -t docker-cicd-pcf:"${docker_version}" -t docker-cicd-pcf:latest "${__dir}"
+docker build --build-arg PCF_CLI_VERSION="${pcf_cli_version}" --pull -t docker-cicd-pcf:"${docker_version}" -t docker-cicd-pcf:latest "${__dir}"
 # docker push mrllsvc/pcf-tools:"${docker_version}"
 # docker push mrllsvc/pcf-tools:latest
